745198827acde01948275ae985556303987e27a9
Aaditya Sahay <aaditya@speechify.com>
2025-05-01 01:18:57 +0530

    uncomment some stuff

--- mobile/src/storage/android.rs
+++ mobile/src/storage/android.rs
@@ -1,233 +1,233 @@
-// use dioxus::mobile::wry::prelude::dispatch;
-// use std::sync::mpsc;
+use dioxus::mobile::wry::prelude::dispatch;
+use std::sync::mpsc;
 
-// // Function to securely store a key-value pair
-// pub fn secure_store(key: &str, value: &str) -> () {
-//     let key_str = key.to_string();
-//     let value_str = value.to_string();
+// Function to securely store a key-value pair
+pub fn secure_store(key: &str, value: &str) -> () {
+    let key_str = key.to_string();
+    let value_str = value.to_string();
 
-//     let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
-//     dispatch(move |env, activity, _webview| {
-//         let result = (|| {
-//             // Get the context
-//             let context = activity.clone();
+    dispatch(move |env, activity, _webview| {
+        let result = (|| {
+            // Get the context
+            let context = activity.clone();
 
-//             // Get the security library classes
-//             let master_key_class = env
-//                 .find_class("androidx/security/crypto/MasterKey$Builder")
-//                 .unwrap();
-//             let encrypted_prefs_class = env
-//                 .find_class("androidx/security/crypto/EncryptedSharedPreferences")
-//                 .unwrap();
+            // Get the security library classes
+            let master_key_class = env
+                .find_class("androidx/security/crypto/MasterKey$Builder")
+                .unwrap();
+            let encrypted_prefs_class = env
+                .find_class("androidx/security/crypto/EncryptedSharedPreferences")
+                .unwrap();
 
-//             // Create master key
-//             let master_key_builder = env
-//                 .new_object(
-//                     &master_key_class,
-//                     "(Landroid/content/Context;)V",
-//                     &[context.into()],
-//                 )
-//                 .unwrap();
+            // Create master key
+            let master_key_builder = env
+                .new_object(
+                    &master_key_class,
+                    "(Landroid/content/Context;)V",
+                    &[context.into()],
+                )
+                .unwrap();
 
-//             // Set key parameters
-//             env.call_method(
-//                 &master_key_builder,
-//                 "setKeyScheme",
-//                 "(I)Landroidx/security/crypto/MasterKey$Builder;",
-//                 &[3i32.into()], // MasterKey.KeyScheme.AES256_GCM
-//             )
-//             .unwrap();
+            // Set key parameters
+            env.call_method(
+                &master_key_builder,
+                "setKeyScheme",
+                "(I)Landroidx/security/crypto/MasterKey$Builder;",
+                &[3i32.into()], // MasterKey.KeyScheme.AES256_GCM
+            )
+            .unwrap();
 
-//             let master_key = env
-//                 .call_method(
-//                     &master_key_builder,
-//                     "build",
-//                     "()Landroidx/security/crypto/MasterKey;",
-//                     &[],
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            let master_key = env
+                .call_method(
+                    &master_key_builder,
+                    "build",
+                    "()Landroidx/security/crypto/MasterKey;",
+                    &[],
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             // Get file name for preferences
-//             let pref_file = env.new_string("secure_prefs").unwrap();
+            // Get file name for preferences
+            let pref_file = env.new_string("secure_prefs").unwrap();
 
-//             // Get encryption schemes
-//             let aes256_scheme = env
-//                 .get_static_field(
-//                     "androidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme",
-//                     "AES256_SIV",
-//                     "Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;",
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            // Get encryption schemes
+            let aes256_scheme = env
+                .get_static_field(
+                    "androidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme",
+                    "AES256_SIV",
+                    "Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;",
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             let aes256_value_scheme = env.get_static_field(
-//                   "androidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme",
-//                   "AES256_GCM",
-//                   "Landroidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme;"
-//               ).unwrap().l().unwrap();
+            let aes256_value_scheme = env.get_static_field(
+                  "androidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme",
+                  "AES256_GCM",
+                  "Landroidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme;"
+              ).unwrap().l().unwrap();
 
-//             // Create encrypted preferences
-//             let encrypted_prefs = env.call_static_method(
-//                   &encrypted_prefs_class,
-//                   "create",
-//                   "(Landroid/content/Context;Ljava/lang/String;Landroidx/security/crypto/MasterKey;Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;Landroidx/security/crypto
-//   /EncryptedSharedPreferences$PrefValueEncryptionScheme;)Landroid/content/SharedPreferences;",
-//                   &[
-//                       context.into(),
-//                       (&pref_file).into(),
-//                       (&master_key).into(),
-//                       (&aes256_scheme).into(),
-//                       (&aes256_value_scheme).into()
-//                   ]
-//               ).unwrap().l().unwrap();
+            // Create encrypted preferences
+            let encrypted_prefs = env.call_static_method(
+                  &encrypted_prefs_class,
+                  "create",
+                  "(Landroid/content/Context;Ljava/lang/String;Landroidx/security/crypto/MasterKey;Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;Landroidx/security/crypto
+  /EncryptedSharedPreferences$PrefValueEncryptionScheme;)Landroid/content/SharedPreferences;",
+                  &[
+                      context.into(),
+                      (&pref_file).into(),
+                      (&master_key).into(),
+                      (&aes256_scheme).into(),
+                      (&aes256_value_scheme).into()
+                  ]
+              ).unwrap().l().unwrap();
 
-//             // Edit preferences
-//             let editor = env
-//                 .call_method(
-//                     &encrypted_prefs,
-//                     "edit",
-//                     "()Landroid/content/SharedPreferences$Editor;",
-//                     &[],
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            // Edit preferences
+            let editor = env
+                .call_method(
+                    &encrypted_prefs,
+                    "edit",
+                    "()Landroid/content/SharedPreferences$Editor;",
+                    &[],
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             // Put the value
-//             let key_jstring = env.new_string(&key_str).unwrap();
-//             let value_jstring = env.new_string(&value_str).unwrap();
+            // Put the value
+            let key_jstring = env.new_string(&key_str).unwrap();
+            let value_jstring = env.new_string(&value_str).unwrap();
 
-//             env.call_method(
-//                 &editor,
-//                 "putString",
-//                 "(Ljava/lang/String;Ljava/lang/String;)Landroid/content/SharedPreferences$Editor;",
-//                 &[(&key_jstring).into(), (&value_jstring).into()],
-//             )
-//             .unwrap();
+            env.call_method(
+                &editor,
+                "putString",
+                "(Ljava/lang/String;Ljava/lang/String;)Landroid/content/SharedPreferences$Editor;",
+                &[(&key_jstring).into(), (&value_jstring).into()],
+            )
+            .unwrap();
 
-//             // Apply changes
-//             env.call_method(&editor, "apply", "()V", &[]).unwrap();
-//         })();
+            // Apply changes
+            env.call_method(&editor, "apply", "()V", &[]).unwrap();
+        })();
 
-//         // Send result back
-//         tx.send(()).unwrap();
-//     });
+        // Send result back
+        tx.send(()).unwrap();
+    });
 
-//     rx.recv().unwrap()
-// }
+    rx.recv().unwrap()
+}
 
-// // Function to retrieve a securely stored value
-// pub fn secure_retrieve(key: &str) -> Option<String> {
-//     let key_str = key.to_string();
-//     let (tx, rx) = mpsc::channel();
+// Function to retrieve a securely stored value
+pub fn secure_retrieve(key: &str) -> Option<String> {
+    let key_str = key.to_string();
+    let (tx, rx) = mpsc::channel();
 
-//     dispatch(move |env, activity, _webview| {
-//         let result = (|| {
-//             // Get the context
-//             let context = activity.clone();
+    dispatch(move |env, activity, _webview| {
+        let result = (|| {
+            // Get the context
+            let context = activity.clone();
 
-//             // Get the security library classes
-//             let master_key_class = env
-//                 .find_class("androidx/security/crypto/MasterKey$Builder")
-//                 .unwrap();
-//             let encrypted_prefs_class = env
-//                 .find_class("androidx/security/crypto/EncryptedSharedPreferences")
-//                 .unwrap();
+            // Get the security library classes
+            let master_key_class = env
+                .find_class("androidx/security/crypto/MasterKey$Builder")
+                .unwrap();
+            let encrypted_prefs_class = env
+                .find_class("androidx/security/crypto/EncryptedSharedPreferences")
+                .unwrap();
 
-//             // Create master key
-//             let master_key_builder = env
-//                 .new_object(
-//                     &master_key_class,
-//                     "(Landroid/content/Context;)V",
-//                     &[context.into()],
-//                 )
-//                 .unwrap();
+            // Create master key
+            let master_key_builder = env
+                .new_object(
+                    &master_key_class,
+                    "(Landroid/content/Context;)V",
+                    &[context.into()],
+                )
+                .unwrap();
 
-//             let master_key = env
-//                 .call_method(
-//                     &master_key_builder,
-//                     "build",
-//                     "()Landroidx/security/crypto/MasterKey;",
-//                     &[],
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            let master_key = env
+                .call_method(
+                    &master_key_builder,
+                    "build",
+                    "()Landroidx/security/crypto/MasterKey;",
+                    &[],
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             // Get file name for preferences
-//             let pref_file = env.new_string("secure_prefs").unwrap();
+            // Get file name for preferences
+            let pref_file = env.new_string("secure_prefs").unwrap();
 
-//             // Get encryption schemes
-//             let aes256_scheme = env
-//                 .get_static_field(
-//                     "androidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme",
-//                     "AES256_SIV",
-//                     "Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;",
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            // Get encryption schemes
+            let aes256_scheme = env
+                .get_static_field(
+                    "androidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme",
+                    "AES256_SIV",
+                    "Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;",
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             let aes256_value_scheme = env.get_static_field(
-//                   "androidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme",
-//                   "AES256_GCM",
-//                   "Landroidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme;"
-//               ).unwrap().l().unwrap();
+            let aes256_value_scheme = env.get_static_field(
+                  "androidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme",
+                  "AES256_GCM",
+                  "Landroidx/security/crypto/EncryptedSharedPreferences$PrefValueEncryptionScheme;"
+              ).unwrap().l().unwrap();
 
-//             // Create encrypted preferences
-//             let encrypted_prefs = env.call_static_method(
-//                   &encrypted_prefs_class,
-//                   "create",
-//                   "(Landroid/content/Context;Ljava/lang/String;Landroidx/security/crypto/MasterKey;Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;Landroidx/security/crypto
-//   /EncryptedSharedPreferences$PrefValueEncryptionScheme;)Landroid/content/SharedPreferences;",
-//                   &[
-//                       context.into(),
-//                       (&pref_file).into(),
-//                       (&master_key).into(),
-//                       (&aes256_scheme).into(),
-//                       (&aes256_value_scheme).into()
-//                   ]
-//               ).unwrap().l().unwrap();
+            // Create encrypted preferences
+            let encrypted_prefs = env.call_static_method(
+                  &encrypted_prefs_class,
+                  "create",
+                  "(Landroid/content/Context;Ljava/lang/String;Landroidx/security/crypto/MasterKey;Landroidx/security/crypto/EncryptedSharedPreferences$PrefKeyEncryptionScheme;Landroidx/security/crypto
+  /EncryptedSharedPreferences$PrefValueEncryptionScheme;)Landroid/content/SharedPreferences;",
+                  &[
+                      context.into(),
+                      (&pref_file).into(),
+                      (&master_key).into(),
+                      (&aes256_scheme).into(),
+                      (&aes256_value_scheme).into()
+                  ]
+              ).unwrap().l().unwrap();
 
-//             // Get the value
-//             let key_jstring = env.new_string(&key_str).unwrap();
-//             let default_value = env.new_string("").unwrap();
+            // Get the value
+            let key_jstring = env.new_string(&key_str).unwrap();
+            let default_value = env.new_string("").unwrap();
 
-//             let value = env
-//                 .call_method(
-//                     &encrypted_prefs,
-//                     "getString",
-//                     "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
-//                     &[(&key_jstring).into(), (&default_value).into()],
-//                 )
-//                 .unwrap()
-//                 .l()
-//                 .unwrap();
+            let value = env
+                .call_method(
+                    &encrypted_prefs,
+                    "getString",
+                    "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
+                    &[(&key_jstring).into(), (&default_value).into()],
+                )
+                .unwrap()
+                .l()
+                .unwrap();
 
-//             // Convert to Rust String
-//             if value.is_null() {
-//                 None
-//             } else {
-//                 let value_str = env
-//                     .get_string((&value).into())
-//                     .unwrap()
-//                     .to_string_lossy()
-//                     .to_string();
-//                 if value_str.is_empty() {
-//                     None
-//                 } else {
-//                     Some(value_str)
-//                 }
-//             }
-//         })();
+            // Convert to Rust String
+            if value.is_null() {
+                None
+            } else {
+                let value_str = env
+                    .get_string((&value).into())
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string();
+                if value_str.is_empty() {
+                    None
+                } else {
+                    Some(value_str)
+                }
+            }
+        })();
 
-//         // Send result back
-//         tx.send(result).unwrap();
-//     });
+        // Send result back
+        tx.send(result).unwrap();
+    });
 
-//     rx.recv().unwrap()
-// }
+    rx.recv().unwrap()
+}
--- mobile/src/storage/mod.rs
+++ mobile/src/storage/mod.rs
@@ -1,67 +1,67 @@
-// // TODO: need to add to the build.gradle file
-// // dependencies {
-// //     implementation "androidx.security:security-crypto:1.1.0-alpha07"
-// // }
-
-// use dioxus::{
-//     hooks::use_signal,
-//     signals::{Readable, Signal, Writable},
-// };
-// use serde::{de::DeserializeOwned, Serialize};
-
-// #[cfg(target_os = "android")]
-// mod android;
-// mod store;
-
-// use store::SecureStore;
-
-// pub fn use_persistent<T: Serialize + DeserializeOwned + Default + 'static>(
-//     // A unique key for the storage entry
-//     key: impl ToString,
-//     // A function that returns the initial value if the storage entry is empty
-//     init: impl FnOnce() -> T,
-// ) -> UsePersistent<T> {
-//     // Use the use_signal hook to create a mutable state for the storage entry
-//     let state = use_signal(move || {
-//         // This closure will run when the hook is created
-//         let key = key.to_string();
-//         let value = SecureStore::get(key.as_str()).unwrap_or_else(init);
-//         StorageEntry { key, value }
-//     });
-
-//     // Wrap the state in a new struct with a custom API
-//     UsePersistent { inner: state }
+// TODO: need to add to the build.gradle file
+// dependencies {
+//     implementation "androidx.security:security-crypto:1.1.0-alpha07"
 // }
 
-// struct StorageEntry<T> {
-//     key: String,
-//     value: T,
-// }
+use dioxus::{
+    hooks::use_signal,
+    signals::{Readable, Signal, Writable},
+};
+use serde::{de::DeserializeOwned, Serialize};
 
-// /// Storage that persists across application reloads
-// pub struct UsePersistent<T: 'static> {
-//     inner: Signal<StorageEntry<T>>,
-// }
+#[cfg(target_os = "android")]
+mod android;
+mod store;
 
-// impl<T> Clone for UsePersistent<T> {
-//     fn clone(&self) -> Self {
-//         *self
-//     }
-// }
+use store::SecureStore;
 
-// impl<T> Copy for UsePersistent<T> {}
+pub fn use_persistent<T: Serialize + DeserializeOwned + Default + 'static>(
+    // A unique key for the storage entry
+    key: impl ToString,
+    // A function that returns the initial value if the storage entry is empty
+    init: impl FnOnce() -> T,
+) -> UsePersistent<T> {
+    // Use the use_signal hook to create a mutable state for the storage entry
+    let state = use_signal(move || {
+        // This closure will run when the hook is created
+        let key = key.to_string();
+        let value = SecureStore::get(key.as_str()).unwrap_or_else(init);
+        StorageEntry { key, value }
+    });
 
-// impl<T: Serialize + DeserializeOwned + Clone + 'static> UsePersistent<T> {
-//     /// Returns a reference to the value
-//     pub fn get(&self) -> T {
-//         self.inner.read().value.clone()
-//     }
+    // Wrap the state in a new struct with a custom API
+    UsePersistent { inner: state }
+}
 
-//     /// Sets the value
-//     pub fn set(&mut self, value: T) {
-//         let mut inner = self.inner.write();
-//         // Write the new value to local storage
-//         SecureStore::set(inner.key.as_str(), &value);
-//         inner.value = value;
-//     }
-// }
+struct StorageEntry<T> {
+    key: String,
+    value: T,
+}
+
+/// Storage that persists across application reloads
+pub struct UsePersistent<T: 'static> {
+    inner: Signal<StorageEntry<T>>,
+}
+
+impl<T> Clone for UsePersistent<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for UsePersistent<T> {}
+
+impl<T: Serialize + DeserializeOwned + Clone + 'static> UsePersistent<T> {
+    /// Returns a reference to the value
+    pub fn get(&self) -> T {
+        self.inner.read().value.clone()
+    }
+
+    /// Sets the value
+    pub fn set(&mut self, value: T) {
+        let mut inner = self.inner.write();
+        // Write the new value to local storage
+        SecureStore::set(inner.key.as_str(), &value);
+        inner.value = value;
+    }
+}
